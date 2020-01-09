4c735b1658e6e24dadcdd7395e9e55d6988c0834
root <root@ip-172-31-37-187.ec2.internal>
2020-01-09 08:31:59 +0000

    Initial COmmit

---
+++ test-database/setup.sql
@@ -0,0 +1,6 @@
+create table directory (user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email TEXT, phone_number TEXT);
+insert into directory (email, phone_number) values ('anuj@devopslearning.com', '+1 888 123 1111');
+insert into directory (email, phone_number) values ('marge@devopslearning.com', '+1 888 123 1112');
+insert into directory (email, phone_number) values ('maggie@devopslearning.com', '+1 888 123 1113');
+insert into directory (email, phone_number) values ('lisa@devopslearning.com', '+1 888 123 1114');
+insert into directory (email, phone_number) values ('bart@devopslearning.com', '+1 888 123 1115');
